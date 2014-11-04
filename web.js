ec60dc05465e565df67cdf72451a2c712d192870
ruizb <benoit.ruiz01@gmail.com>
2014-11-04 23:21:39 +0100

    Change express version for web server

--- web.js
+++ web.js
@@ -1,7 +1,13 @@
-var gzippo = require('gzippo');
-var express = require('express');
+//var gzippo = require('gzippo');
+//var express = require('express');
+//var app = express();
+//
+//app.use(express.logger('dev'));
+//app.use(gzippo.staticGzip("" + __dirname + "/bin"));
+//app.listen(process.env.PORT || 5000);
+
+var express = require('express')
 var app = express();
 
-app.use(express.logger('dev'));
-app.use(gzippo.staticGzip("" + __dirname + "/bin"));
-app.listen(process.env.PORT || 5000);
+app.set('port', (process.env.PORT || 5000))
+app.use(express.static(__dirname + '/bin'))
