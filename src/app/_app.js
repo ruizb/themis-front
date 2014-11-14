71f169f1bfc037efa425e41df924d925a828d1c6
Adeline Blaclard <adeline.blaclard@gmail.com>
2014-11-14 15:17:48 +0100

    modif on _app

--- src/app/_app.js
+++ src/app/_app.js
@@ -5,13 +5,8 @@ angular
     'themis.login',
     'themis.home',
     'themis.enqueteurs',
-<<<<<<< HEAD
-	'themis.grades',
 	'themis.tribunals',
-    'ui.router'
-=======
     'themis.ranks',
     'ui.router',
     'angularUtils.directives.dirPagination'
->>>>>>> master
   ]);
