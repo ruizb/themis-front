c3b48d2401aa8dabcf5a12d326f6f3aeb7c437db
paolaquettier <paolaquettier@gmail.com>
2014-11-14 15:03:15 +0100

    Change of feature Mission - Pop up to delete, pagination and Entity

--- src/app/missions/Mission.js
+++ src/app/missions/Mission.js
@@ -1,18 +1,18 @@
 angular
   .module('themis.missions')
-  .factory('Mission', function (Entity, $q, $http) {
+  .factory('Mission', function ($q, $http) {
   
     var Mission = function () {
-      Entity.call(this);
+      Entity.call(this, $http, $q);
       this.url += '/missions';
     };
     Mission.prototype = Object.create(Entity.prototype);
 	
 	Mission.prototype.getAll = function () {
-      var deferred = $q.defer();
+      var deferred = this.$q.defer();
 
       var missions = [];
-      for (var i = 0; i < 4; i++) {
+      for (var i = 0; i < 50; i++) {
         missions.push({
           id: i,
           label: "Chat"
